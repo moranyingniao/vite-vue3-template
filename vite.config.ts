ed1406afb425c1b4bab238858012d432761d5978
陈国庆 <63732331+moranyingniao@users.noreply.github.com>
2022-09-23 17:14:13 +0800

    Update vite.config.ts
    更新代理

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  devServer: {
-    // 设置代理
+   server: {
     proxy: {
-      "/api": {
-        target: "http://127.0.0.1:3000", // 域名
-        // ws: true, // 是否启用websockets
-        changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
-        pathRewrite: {
-          "^/api": "/",
-        },
+      // 接口地址代理
+      '/demo': {
+        target: 'http://192.168.0.238:8088', // 接口的域名
+        secure: false, // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        rewrite: path => path.replace(/^\/demo/, '')
       },
-    //   "/muxiao": {
-    //     target: "https://v.api.aa1.cn/api/api-wenan-gaoxiao", // 域名
-    //     // ws: true, // 是否启用websockets
-    //     changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
-    //     pathRewrite: {
-    //       "^/muxiao": "/",
-    //     },
-    //   },
-    },
+    }
+  },
   }
 })
 
