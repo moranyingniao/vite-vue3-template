11b6cb2c28c0bb0490cafe76769bba16c7ff8581
17628191484 <2306727047@qq.com>
2022-09-23 16:47:14 +0800

    更新反向代理

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,28 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  devServer: {
+    // 设置代理
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000", // 域名
+        // ws: true, // 是否启用websockets
+        changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        pathRewrite: {
+          "^/api": "/",
+        },
+      },
+    //   "/muxiao": {
+    //     target: "https://v.api.aa1.cn/api/api-wenan-gaoxiao", // 域名
+    //     // ws: true, // 是否启用websockets
+    //     changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+    //     pathRewrite: {
+    //       "^/muxiao": "/",
+    //     },
+    //   },
+    },
+  }
 })
+
+
