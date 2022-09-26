fe2254e8859129cc0af736751d97618edbe1a28d
陈国庆 <63732331+moranyingniao@users.noreply.github.com>
2022-09-26 11:03:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-   server: {
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  server: {
     proxy: {
       // 接口地址代理
       '/demo': {
